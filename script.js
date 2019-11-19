49f2555a19992b9e7fd622775cfc4a8da7fb7fa2
Rylie Nellis <rnellis@flemingcollege.ca>
2019-11-18 20:19:34 -0500

    google maps

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-<script>
-// This is a JS file
-</script>
+// Google maps clip for footer - Rylie //
+function myMap() { 
+var mapID= { 
+    center: new google.maps.LatLng(44.355000, -78.742930), 
+    zoom: 5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"), mapID);
+}
+
