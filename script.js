e3c946be6d60f70c69f288ca41681d9d3142cc9f
Dan Noakes <dnoakes@flemingcollege.ca>
2019-12-03 11:55:04 -0500

    Added Images, made a very rough version of the slideshow. CSS needs to be modified

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Google maps clip for footer - Rylie //
+// Google maps clip for footer - Owner: Rylie //
 function myMap() { 
 var mapID= { 
     center: new google.maps.LatLng(44.355000, -78.742930), 
@@ -7,3 +7,33 @@ var mapID= {
 var map = new google.maps.Map(document.getElementById("googleMap"), mapID);
 }
 
+// Moving Images - Owner: Dan //
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// End of Moving Images - Owner: Dan //
